394e3f6fdb9278658c69e68eba3febc3b78d451b
KimNamKwang <durante0916@gmail.com>
2023-01-06 14:43:42 +0900

    first commit

---
+++ src/main/java/com/holy_moly_final/holy_moly_final/HolyMolyFinalApplication.java
@@ -0,0 +1,13 @@
+package com.holy_moly_final.holy_moly_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolyMolyFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolyMolyFinalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/holy_moly_final/holy_moly_final/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.holy_moly_final.holy_moly_final;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HolyMolyFinalApplication.class);
+	}
+
+}
